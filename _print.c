16c656d086393c38f20230f72240fc540122f4b8
balemansteve <balemansteve@gmail.com>
2024-03-16 06:55:15 +0000

    change if

--- _print.c
+++ _print.c
@@ -25,8 +25,6 @@ int _printf(const char *format, ...)
 		if (*ptr == '%')
 		{
 			ptr++;
-			if (*ptr == '\0')
-				break;
 
 			if (*ptr == '%')
 			{
@@ -34,6 +32,9 @@ int _printf(const char *format, ...)
 				ptr++;
 				continue;
 			}
+
+			if (*ptr == '\0')
+				break;
 
 			switch (*ptr)
 			{
