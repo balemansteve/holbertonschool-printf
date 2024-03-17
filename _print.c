cfe8b5d767751a643a4b6f55d85959b84f203df8
balemansteve <balemansteve@gmail.com>
2024-03-17 12:10:54 +0000

    print % repair

--- _print.c
+++ _print.c
@@ -58,7 +58,6 @@ int _printf(const char *format, ...)
 						count += write(1, "%", 1);
 						break;
 					default:
-						count += write(1, "%", 1);
 						count += write(1, ptr, 1);
 						break;
 				}
